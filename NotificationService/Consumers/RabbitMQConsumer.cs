e3bee7768566ceb56b2021722857685d54e09859
Chris Beaver <chrisb@pinpointcomms.com.au>
2017-04-17 12:15:00 +1000

    consume message

--- NotificationService/Consumers/RabbitMQConsumer.cs
+++ NotificationService/Consumers/RabbitMQConsumer.cs
@@ -1,13 +1,17 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.MessagePatterns;
 using System;
+using System.Configuration;
+
 
 namespace NotificationService
 {
     public class RabbitMQConsumer : IQueueConsumer
     {
 
-        private const string EXCHANGENAME = "com.cwbeaver_exchange";
-        private const string CARDPAYMENTQUEUENAME = "NotificationTopic_Queue";
+        private const string EXCHANGE_NAME = "com.cwbeaver_exchange";
+        private const string QUEUE_NAME = "NotificationTopic_Queue";
 
         private static ConnectionFactory _factory;
         private static IConnection _connection;
@@ -15,12 +19,36 @@ namespace NotificationService
 
         public void CreateConnection()
         {
-            throw new NotImplementedException();
+            _factory = new ConnectionFactory
+            {
+                HostName = ConfigurationSettings.AppSettings["QueueHostName"].ToString(),
+                UserName = ConfigurationSettings.AppSettings["QueueUserName"].ToString(),
+                Password = ConfigurationSettings.AppSettings["QueuePassword"].ToString(),
+            };
+
+            _connection = _factory.CreateConnection();
+
+            _model = _connection.CreateModel();
+
+            _model.ExchangeDeclare(EXCHANGE_NAME, "topic");
+
+            _model.QueueDeclare(QUEUE_NAME, true, false, false, null);
+            _model.QueueBind(QUEUE_NAME, EXCHANGE_NAME, "notification.email");
+            _model.BasicQos(0, 1, false);
         }
 
         public void ProcessMessages()
         {
-            throw new NotImplementedException();
+            Subscription subscription = new Subscription(_model, QUEUE_NAME, false); 
+
+            while (true)
+            {
+                BasicDeliverEventArgs delArgs = subscription.Next();
+
+                var message = System.Text.Encoding.UTF8.GetString(delArgs.Body);
+                Console.WriteLine(message);
+                subscription.Ack(delArgs);
+            }
         }
     }
 }
